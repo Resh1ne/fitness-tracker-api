1c2dc38073901dddfb7854c29540bece353c5b70
super <daniilpototskiy22@gmail.com>
2025-11-13 02:11:21 +0300

    feat(auth): implement registration and login endpoints

---
+++ src/main/java/com/example/fitnesstracker/service/impl/AuthenticationService.java
@@ -0,0 +1,55 @@
+package com.example.fitnesstracker.service.impl;
+
+import com.example.fitnesstracker.dto.AuthenticationResponse;
+import com.example.fitnesstracker.dto.LoginRequest;
+import com.example.fitnesstracker.dto.RegisterRequest;
+import com.example.fitnesstracker.entity.User;
+import com.example.fitnesstracker.entity.enums.Role;
+import com.example.fitnesstracker.repository.UserRepository;
+import com.example.fitnesstracker.service.jwt.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        if (userRepository.findByEmail(request.getEmail()).isPresent()) {
+            throw new IllegalStateException("Email already taken");
+        }
+
+        var user = User.builder()
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+
+        userRepository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse login(LoginRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
+        var user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new IllegalStateException("User not found after authentication"));
+
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+}
